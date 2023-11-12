b5efcd2064e18ceee3614b6f3851f623c8542288
Valdimir Gavrilyuk <voovius@yahoo.com>
2023-11-12 23:09:08 +0300

    lesson3/task#2 done

---
+++ lesson003/homework/src/task2/AvgCalc.java
@@ -0,0 +1,20 @@
+package task2;
+
+public class AvgCalc {
+    int a0 = 0, a1 = 1, a2 = 2, a3 = 3, a4 = 4,
+            a5 = 5, a6 = 6, a7 = 7, a8 = 8, a9 = 9;
+
+    int avg;
+    int rem;
+
+    int calcAvg() {
+        avg = a0 + a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 + a9;
+        rem = avg % 10;
+
+        return avg /= 10;
+    }
+
+    void print() {
+        System.out.printf("average: %d\nremaining: %d", avg, rem);
+    }
+}
---
+++ lesson003/homework/src/task2/Task2Test.java
@@ -0,0 +1,10 @@
+package task2;
+
+public class Task2Test {
+    public static void main(String[] args) {
+        AvgCalc ac = new AvgCalc();
+
+        ac.calcAvg();
+        ac.print();
+    }
+}
