31dde123708786129e680a6d6efbeae773909512
Valdimir Gavrilyuk <voovius@yahoo.com>
2023-11-13 21:57:38 +0300

    update - exchangeRate

--- lesson003/homework/src/test1/Robot.java
+++ lesson003/homework/src/test1/Robot.java
@@ -6,9 +6,32 @@ public class Robot {
     private String make;
     private String model;
 
+    private double amount;
+    private double exchangeRate = 1.2;
+
+
     public Robot(String make, String model) {
         this.make = make;
         this.model = model;
+    }
+
+    public  double covertToEUR() {
+        return exchangeRate * amount;
+    }
+    public double getAmount() {
+        return amount;
+    }
+
+    public double getExchangeRate() {
+        return exchangeRate;
+    }
+
+    public void setExchangeRate(double exchangeRate) {
+        this.exchangeRate = exchangeRate;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
     }
 
     public String getMake() {
@@ -35,5 +58,12 @@ public class Robot {
         Robot robot = new Robot("The best", "R001");
 
         robot.sayHello();
+        robot.setAmount(1000.0);
+
+        double euros = robot.covertToEUR();
+
+        System.out.printf("I've had %10.2f USD, it is equivalent of %10.2f EUR (the rate is %10.2f)\n",
+                                robot.getAmount(),
+                                euros,  robot.getExchangeRate());
     }
 }
