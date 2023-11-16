3262a35b17f96b200f09cf85c9bdd28401b0270d
Voovius <voovius@yahoo.com>
2023-11-16 11:29:36 +0300

    Minor update

--- lesson004/src/homework/Book.java
+++ lesson004/src/homework/Book.java
@@ -97,7 +97,7 @@ public class Book {
         book2.print();
         book3.print();
 
-        System.out.println("\nBook\tId\tAuthor\tPages\tAvailability");
+        System.out.println("\nBook\tID\tAuthor\tPages\tAvailability");
         book.printOneLine();
         book1.printOneLine();
         book2.printOneLine();
