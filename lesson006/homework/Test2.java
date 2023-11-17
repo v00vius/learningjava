b43e59d77ec45d08edb05b20e85466081e23df53
Vladimir Gavrilyuk <voovius@yahoo.com>
2023-11-18 00:24:57 +0300

    lesson006 practice

---
+++ lesson006/homework/Test2.java
@@ -0,0 +1,12 @@
+import utils.UserInterface;
+
+public class Test2 {
+    public static void main(String[] args) {
+        UserInterface ui = new UserInterface();
+        int a = ui.getInt("Enter the number x: ");
+        int b = ui.getInt("Enter the number y: ");
+        int c = ui.getInt("Enter the number z: ");
+
+        System.out.println("Numbers: " + a + ", " + b + ", " + c);
+    }
+}
