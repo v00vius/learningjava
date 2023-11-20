f456f9693920915b3a1b894cf16e982ed249939b
Vladimir Gavrilyuk <voovius@yahoo.com>
2023-11-20 23:49:36 +0300

    lesson007 task2 has updated

--- lesson007/classwork/task2/Prime.java
+++ lesson007/classwork/task2/Prime.java
@@ -19,7 +19,7 @@ public class Prime {
     }
 
     boolean isPrime() {
-        for (int i = 2; i <=  number / 2; ++i) {
+        for (int i = 2; i <=  (int)Math.round(Math.sqrt(number)); ++i) {
             if(number % i == 0) {
                 divider = i;
 
