99cafcb0bf67a4780673e6ea03e0808ad23cf5f8
Vladimir Gavrilyuk <voovius@yahoo.com>
2023-11-22 00:09:36 +0300

    lesson007: loop: task 1  done

---
+++ lesson007/homework/loops/Calc1.java
@@ -0,0 +1,25 @@
+package loops;
+
+import utils.UserInterface;
+
+public class Calc1 {
+    private int number;
+
+    public Calc1(int number) {
+        this.number = number;
+    }
+
+    void power3() {
+        for (int i = 1; i <= number ; ++i) {
+            System.out.println(i + " to power 3 = " + i * i * i);
+        }
+    }
+
+    public static void main(String[] args) {
+        UserInterface ui = new UserInterface();
+
+        Calc1 calc = new Calc1(ui.getInt("Enter a number > 0: "));
+
+        calc.power3();
+    }
+}
