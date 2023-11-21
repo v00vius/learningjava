fe64265684534cbe10e408bed89f8fae56c78aec
Vladimir Gavrilyuk <voovius@yahoo.com>
2023-11-22 00:48:08 +0300

    lesson007: loop2: task 3  done

---
+++ lesson007/homework/loops2/Task3.java
@@ -0,0 +1,36 @@
+package loops2;
+
+import utils.UserInterface;
+
+public class Task3 {
+    private String hello;
+    private int attempts;
+
+    public Task3(String hello) {
+        this.hello = hello;
+        attempts = 0;
+    }
+    public boolean isEqual(String word) {
+        boolean ok =  hello.equals(word.trim());
+
+        if(!ok)
+            ++attempts;
+
+        return ok;
+    }
+
+    public int getAttempts() {
+        return attempts;
+    }
+
+    public static void main(String[] args) {
+        UserInterface ui = new UserInterface();
+
+        Task3 hello = new Task3("hello");
+
+        while(!hello.isEqual(ui.getString("Hello!\n")))
+            ;
+
+        System.out.println("Total attempts: " + (hello.getAttempts() + 1));
+    }
+}
