5b7045f6f69435b9629abfc2b8a1a4ff3641cc4e
Vladimir Gavrilyuk <voovius@yahoo.com>
2023-12-05 17:54:49 +0300

    Simple hardware store project

---
+++ lesson013/homework/harwarestore/Service.java
@@ -0,0 +1,13 @@
+package harwarestore;
+
+public class Service {
+    private Warehouse store;
+
+    public Service(Warehouse store) {
+        this.store = store;
+    }
+
+    public void loadStore() {
+
+    }
+}
