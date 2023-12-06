8d949f413f14e61e6dad2bced0cdc00386d4e4c5
Vladimir Gavrilyuk <voovius@yahoo.com>
2023-12-06 11:58:58 +0300

    added the data model

--- lesson013/homework/harwarestore/WarehouseService.java
+++ lesson013/homework/harwarestore/WarehouseService.java
@@ -4,19 +4,19 @@ import utils.UserInterface;
 
 public class WarehouseService {
     private Warehouse store;
-    private UserInterface ui;
+    private UserInterface userInterface;
 
     public WarehouseService(Warehouse store) {
         this.store = store;
-        ui = new UserInterface();
+        userInterface = new UserInterface();
     }
 
     public void loadWarehouse() {
         while(true) {
-            ui.message("*** [Warehouse '" + store.getName() + "'] ***\n");
-            ui.message(store + "\n");
+            userInterface.message("*** [Warehouse '" + store.getName() + "'] ***\n");
+            userInterface.message(store + "\n");
 
-            int choice = ui.menuChoice(new String[] {
+            int choice = userInterface.menuChoice(new String[] {
                     "Load an instrument into the warehouse",
                     "Load a kit into the warehouse"
             });
@@ -30,55 +30,55 @@ public class WarehouseService {
                 loadKit();
         }
 
-        ui.message("Bye!\n");
+        userInterface.message("Bye!\n");
     }
 
     public void loadInstrument() {
-        String instrumentName = ui.getString("Give the name of the new instrument: ");
+        String instrumentName = userInterface.getString("Give the name of the new instrument: ");
 
         if(instrumentName.isEmpty()) {
-            ui.message("Operation canceled!\n");
+            userInterface.message("Operation canceled!\n");
             return;
         }
 
         Instrument tool = store.findInstrumentByName(instrumentName);
 
         if(tool != null) {
-            ui.message("Instrument '" + instrumentName + "' already exists in the Warehouse.\n");
+            userInterface.message("Instrument '" + instrumentName + "' already exists in the Warehouse.\n");
             return;
         }
 
         tool = new Instrument(instrumentName);
-        ui.message("You've entered the new instrument '" + tool + "'\n");
+        userInterface.message("You've entered the new instrument '" + tool + "'\n");
         store.append(tool);
     }
 
     public void loadKit() {
-        String kitName = ui.getString("Give the name of the new Kit: ");
+        String kitName = userInterface.getString("Give the name of the new Kit: ");
 
         if(kitName.isEmpty()) {
-            ui.message("Operation canceled!\n");
+            userInterface.message("Operation canceled!\n");
             return;
         }
 
-        String instrument1Name = ui.getString("Give the name of the first (existing in the warehouse) instrument: ");
+        String instrument1Name = userInterface.getString("Give the name of the first (existing in the warehouse) instrument: ");
         Instrument tool1 = store.findInstrumentByName(instrument1Name);
 
         if(tool1 == null) {
-            ui.message("Instrument '" + instrument1Name + "' doesn't exist in the Warehouse.\n");
+            userInterface.message("Instrument '" + instrument1Name + "' doesn't exist in the Warehouse.\n");
             return;
         }
 
-        String instrument2Name = ui.getString("Give the name of the second (existing in the warehouse) instrument: ");
+        String instrument2Name = userInterface.getString("Give the name of the second (existing in the warehouse) instrument: ");
         Instrument tool2 = store.findInstrumentByName(instrument2Name);
 
         if(tool2 == null) {
-            ui.message("Instrument '" + instrument2Name + "' doesn't exist in the Warehouse.\n");
+            userInterface.message("Instrument '" + instrument2Name + "' doesn't exist in the Warehouse.\n");
             return;
         }
 
         InstrumentKit kit = new InstrumentKit(kitName, tool1, tool2);
-        ui.message("You've entered the new Kit: " + kit + "\n");
+        userInterface.message("You've entered the new Kit: " + kit + "\n");
         store.append(kit);
     }
 }
