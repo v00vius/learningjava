676df0d39509264e71ea79d0b950dac7c23162f9
Vladimir Gavrilyuk <voovius@yahoo.com>
2023-12-07 18:04:04 +0300

    Test git push over ssh

--- lesson013/homework/harwarestore/App.java
+++ lesson013/homework/harwarestore/App.java
@@ -9,12 +9,13 @@ public class App {
 
         store.append(tool1);
         store.append(tool2);
-        InstrumentKit kit1 = new InstrumentKit("Kit A", tool1, tool2);
 
+        InstrumentKit kit1 = new InstrumentKit("Kit A", tool1, tool2);
         store.append(kit1);
 
         tool1 = new Instrument("Handsaw");
         tool2 = new Instrument("Combination pliers");
+
         store.append(tool1);
         store.append(tool2);
 
