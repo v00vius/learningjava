92599813e256a45d3b5e6eee3c6efe961b21e42a
Vladimir Gavrilyuk <voovius@yahoo.com>
2023-12-15 21:59:48 +0300

    classworking

---
+++ lesson018/classwork/src/inheritance/Dog.java
@@ -0,0 +1,18 @@
+package inheritance;
+
+public class Dog {
+
+    private String name;
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void voice(){
+        System.out.println("GAV!!!");
+    }
+}
---
+++ lesson018/classwork/src/inheritance/HomeAnimals.java
@@ -0,0 +1,18 @@
+package inheritance;
+
+public class HomeAnimals {
+    public static void main(String[] args) {
+        Dog dog1 = new Dog("Barbos");
+        Dog dog2 = new Dog("Palkan");
+
+
+        Cat cat1 = new Cat("Murzik");
+        Cat cat2 = new Cat("Barsik");
+
+        Dog[] dogs = {dog1, dog2};
+        Cat[] cats = {cat1, cat2};
+
+
+
+    }
+}
