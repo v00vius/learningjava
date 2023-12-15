ac9bb57d0580f8b3c6647b6c8a258ba3967f4490
Vladimir Gavrilyuk <voovius@yahoo.com>
2023-12-15 22:02:32 +0300

    classworking

--- lesson018/classwork/src/inheritance/HomeAnimals.java
+++ lesson018/classwork/src/inheritance/HomeAnimals.java
@@ -12,7 +12,16 @@ public class HomeAnimals {
         Dog[] dogs = {dog1, dog2};
         Cat[] cats = {cat1, cat2};
 
+        System.out.println(">>> Dogs <<<");
 
+        for (int i = 0; i < dogs.length; ++i) {
+            System.out.println("  " + (i + 1) + ") " + dogs[i].getName());
+        }
 
+        System.out.println("\n>>> Cats <<<");
+
+        for (int i = 0; i < cats.length; ++i) {
+            System.out.println("  " + (i + 1) + ") " + cats[i].getName());
+        }
     }
 }
