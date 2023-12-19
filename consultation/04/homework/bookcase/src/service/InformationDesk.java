8f531dc4e1fde5b41d379e2b7131a698015086a4
Vladimir Gavrilyuk <voovius@yahoo.com>
2023-12-19 12:23:05 +0300

    Information Desk

--- consultation/04/homework/bookcase/src/service/InformationDesk.java
+++ consultation/04/homework/bookcase/src/service/InformationDesk.java
@@ -1,7 +1,26 @@
 package service;
 
+import entity.BookCase;
+
 public class InformationDesk {
 //  User information desk
 //      menu
 //          set/take book, print contents of a bookcase, etc.)
+    BookCase bookCase;
+    BookCaseService bookCaseService;
+
+    public InformationDesk() {
+        bookCaseService = new BookCaseService();
+    }
+
+    public void informationDesk() {
+        System.out.println("Information Desk");
+        System.out.println("1. Set Book");
+        System.out.println("2. Take Book");
+        System.out.println("3. Print Bookcase");
+        System.out.println("4. Swap Books");
+        System.out.println("5. Move Book to Shelf");
+        System.out.println("6. Move Book to Shelf");
+        System.out.println("7. Exit");
+    }
 }
