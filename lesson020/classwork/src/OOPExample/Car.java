c4adb7d92e024708badb7bdec66c53a50261536f
Vladimir Gavrilyuk <voovius@yahoo.com>
2023-12-21 21:16:31 +0300

    update

--- lesson020/classwork/src/OOPExample/Car.java
+++ lesson020/classwork/src/OOPExample/Car.java
@@ -1,6 +1,6 @@
 package OOPExample;
 
-public class Car extends MotorizedVehicle{
+public class    Car extends MotorizedVehicle{
     public Car(String model, Integer speed, Engine engine, GearShift gearShift) {
         super(model, speed, engine, gearShift);
     }
