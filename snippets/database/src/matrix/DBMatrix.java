742ebd80835d96ab3b40a8154d4a96661527e1c4
Vladimir Gavrilyuk <voovius@yahoo.com>
2024-02-13 17:06:33 +0300

    MariaDB test

--- snippets/database/src/matrix/DBMatrix.java
+++ snippets/database/src/matrix/DBMatrix.java
@@ -1,6 +1,7 @@
 package matrix;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBMatrix implements Matrix {
@@ -25,7 +26,7 @@ public void init()
 }
 private void createSequence()
 {
-        var sql = "CREATE SEQUENCE sq_" + name + " INCREMENT=1 START=0 CACHE=1000 NOCYCLE";
+        var sql = "CREATE SEQUENCE sq_" + name + " MINVALUE=0 START=0 INCREMENT=1 CACHE=1000 NOCYCLE";
 
         try (var st = connection.prepareStatement(sql)) {
                 int rc = st.executeUpdate();
@@ -134,6 +135,64 @@ public void mul(int i, int j, double amount)
                 System.out.println(e.getMessage());
         }
 }
+//---
+private static Connection initDatabaseConnection() throws SQLException {
+        System.out.println("Connecting to the database...");
+        var connection = DriverManager.getConnection(
+                "jdbc:mariadb://127.0.0.1:60543/demo",
+                "user",
+                "123456"
+        );
 
+        connection.setAutoCommit(false);
+        System.out.println("Connection valid: " + connection.isValid(5));
 
+        return connection;
+}
+
+private static void closeDatabaseConnection(Connection connection) throws SQLException {
+        System.out.println("Closing database connection...");
+        connection.close();
+        System.out.println("Connection valid: " + connection.isValid(5));
+}
+
+public static void main(String[] args) throws SQLException
+{
+        Connection connection = null;
+
+        try {
+                connection = initDatabaseConnection();
+                var size = 1_000;
+                var matrix = new DBMatrix(connection, "m01", size, size);
+
+                matrix.init();
+                connection.commit();
+
+                for (int i = 0; i < size; i++) {
+                        for (int j = 0; j < size; j++) {
+                                matrix.set(i, j, (double) i * size + j);
+                        }
+
+                        connection.commit();
+                }
+
+                System.out.printf("(%d,%d) = %f\n", size-1, size-1, matrix.get(size-1, size-1));
+
+//                for (int i = 0; i < size; i++) {
+//                        for (int j = 0; j < size; j++) {
+//                                var value = matrix.get(i, j);
+//
+//                                System.out.printf("(%d,%d) = %f\n", i, j, value);
+//                        }
+//
+////                        connection.commit();
+//                }
+
+        } catch (SQLException e) {
+                System.out.println(e.getMessage());
+        } finally {
+                closeDatabaseConnection(connection);
+        }
+
+}
 }
