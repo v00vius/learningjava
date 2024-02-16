85f603a4fd4215eadbdaa751b19f5b2e24ade308
Vladimir Gavrilyuk <voovius@yahoo.com>
2024-02-16 22:54:38 +0300

    backup

--- lesson039/classwork/src/utils/Power2.java
+++ lesson039/classwork/src/utils/Power2.java
@@ -3,6 +3,9 @@ package utils;
 public class Power2 {
 static public boolean isPower2(int x)
 {
+        if(x == 0)
+                return true;
+
         int count = 0;
 
         x = Math.abs(x);
@@ -14,7 +17,7 @@ static public boolean isPower2(int x)
                 x >>>= 1;
         }
 
-        return count == 1 || count == 0;
+        return count == 1;
 }
 
 public static void main(String[] args)
