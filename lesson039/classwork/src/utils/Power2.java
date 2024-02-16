828f5b4cc895462102226dd9a76f0e3d54da838a
Vladimir Gavrilyuk <voovius@yahoo.com>
2024-02-16 22:47:12 +0300

    backup

---
+++ lesson039/classwork/src/utils/Power2.java
@@ -0,0 +1,22 @@
+package utils;
+
+public class Power2 {
+static public boolean isPower2(int x)
+{
+        int count = 0;
+
+        while(x > 0) {
+                if((x & 0x01) == 1)
+                        ++count;
+
+                x >>>= 1;
+        }
+
+        return count == 1 || count == 0;
+}
+
+public static void main(String[] args)
+{
+        System.out.println(isPower2(32));
+}
+}
---
+++ lesson039/classwork/src/utils/Power2Test.java
@@ -0,0 +1,29 @@
+package utils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Power2Test {
+
+int[] test1 = {1,2,4,8,16};
+int[] test2 = {3,5,6,7};
+@Test
+public void isPower2test1()
+{
+        for (int x : test1) {
+                boolean actual = Power2.isPower2(x);
+
+                assertTrue(actual);
+        }
+}
+@Test
+public void isPower2test2()
+{
+        for (int x : test2) {
+                boolean actual = Power2.isPower2(x);
+
+                assertFalse(actual);
+        }
+}
+}
